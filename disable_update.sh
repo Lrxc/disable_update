a0a0f5f481bb5dd8de52cf47af2eb1364f23f186
wangxin <lrxc1117@gmail.com>
2026-09-21 16:45:20 +0800

    更新path

--- disable_update.sh
+++ disable_update.sh
@@ -3,8 +3,15 @@ set -euo pipefail
 
 # 需要处理的路径
 readonly PATHS=(
-	"/Users/lrxc/logs/test1"
-	"/Users/lrxc/logs/test3"
+	"$HOME/Library/Application Support/Microsoft/EdgeUpdater"
+	"$HOME/Library/Microsoft/MicrosoftSoftwareUpdate"
+	"$HOME/Library/Application Support/Google/GoogleUpdater"
+	"$HOME/Library/Google/GoogleSoftwareUpdate"
+	"$HOME/Library/Application Support/Caches/apifox-updater"
+	"$HOME/Library/Caches/com.anthropic.claudefordesktop.ShipIt"
+	"$HOME/Library/Application Support/Claude-3p/vm_bundles"
+	"$HOME/Library/Caches/com.google.antigravity-ide.ShipIt"
+	"$HOME/Library/Containers/com.kingsoft.wpsoffice.mac/Data/.kingsoft/WpsUpdate"
 )
 
 # 属主配置
@@ -19,7 +26,8 @@ set_owner() {
 			echo "\t新建: $path"
 			sudo mkdir -p "$path"
 		fi
-		echo "$action: $path -> $owner"
+		# echo "$action: $path -> $owner"
+		echo "$action: $(realpath "$path") -> $owner"
 		sudo chown "$owner" "$path"
 	done
 }
